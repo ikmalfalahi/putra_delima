9084f79faeb07dd2694268102c4fb417a01484ff
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-10-26 12:22:47 +0700

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,13 @@
-// === Supabase Init ===
-const supabaseUrl = "https://pnxdnbsyvjilvkeoltxo.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBueGRuYnN5dmppbHZrZW9sdHhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0Mzc5MDgsImV4cCI6MjA3NzAxMzkwOH0.em7RJ7jtg2FjTKKotO4C7sA0BFTjPbjF4Rpa4HQZ--M";
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+window.addEventListener("load", () => {
+  if (typeof supabase === "undefined") {
+    console.error("⚠️ Supabase library belum dimuat!");
+    return;
+  }
 
+  const SUPABASE_URL = "https://pnxdnbsyvjilvkeoltxo.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBueGRuYnN5dmppbHZrZW9sdHhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0Mzc5MDgsImV4cCI6MjA3NzAxMzkwOH0.em7RJ7jtg2FjTKKotO4C7sA0BFTjPbjF4Rpa4HQZ--M";
+
+  const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  window.supabase = client;
+  console.log("✅ Supabase client siap digunakan");
+});
