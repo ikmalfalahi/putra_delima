f779d6b911a10943eed8f6ba7f503af5d170ea7c
ikmalfalahi <ikmalfalahi99@gmail.com>
2025-10-26 09:53:52 +0700

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,4 +1,5 @@
 // === Supabase Init ===
-const supabaseUrl = "https://YOUR-PROJECT.supabase.co";
-const supabaseKey = "YOUR-ANON-KEY";
+const supabaseUrl = "https://pnxdnbsyvjilvkeoltxo.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBueGRuYnN5dmppbHZrZW9sdHhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0Mzc5MDgsImV4cCI6MjA3NzAxMzkwOH0.em7RJ7jtg2FjTKKotO4C7sA0BFTjPbjF4Rpa4HQZ--M";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
